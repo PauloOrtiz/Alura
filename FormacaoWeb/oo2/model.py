54abda335562244832d1fac871d80ebf162c6520
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2022-05-29 20:07:18 -0300

    finish course OO2

--- FormacaoWeb/oo2/model.py
+++ FormacaoWeb/oo2/model.py
@@ -5,7 +5,7 @@ class Programa:
         self._likes = 0
 
     @property
-    def likes (self):
+    def likes(self):
         return self._likes
 
     def dar_like(self):
@@ -41,17 +41,36 @@ class Serie(Programa):
         return "{} - {} - {} tp - {} likes".format(self._nome, self.ano, self.temporada, self._likes)
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+
+
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
+atlanta = Serie('atlanta', 2016, 2)
+tmep = Filme('Todo mundo em panico', 1999, 100)
+demolidor = Serie("Demolidor", 2016, 3)
 vingadores.dar_like()
 vingadores.dar_like()
 
 print("Nome: {} - Ano {} - Duracao:{} - Likes {}".format(vingadores.nome, vingadores.ano, vingadores.duracao, vingadores.likes))
-atlanta = Serie('atlanta', 2016, 2)
 atlanta.dar_like()
 
 print("Nome: {} - Ano {} - Temporadas:{} - Likes : {}".format(atlanta.nome, atlanta.ano, atlanta.temporada, atlanta.likes))
 
-filmes_e_serie = [vingadores, atlanta]
+filmes_e_serie = [vingadores, atlanta, demolidor, tmep]
 
-for programa in filmes_e_serie:
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_serie)
+
+print("Tamanho do playlist: {}".format(len(playlist_fim_de_semana)))
+
+
+for programa in playlist_fim_de_semana:
     print(programa)
