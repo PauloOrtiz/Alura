9a97c10b1529f9bd03492207e225f56d36807dfd
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2022-05-29 11:38:24 -0300

    Update

--- FormacaoWeb/oo2/model.py
+++ FormacaoWeb/oo2/model.py
@@ -1,48 +1,45 @@
-class Filme:
-    def __init__(self, nome, ano , duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def likes (self):
-        return self.__likes
+        return self._likes
 
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return "{} - {} - {} likes".format(self._nome, self.ano, self._likes)
 
 
-class Serie:
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return "{} - {} - {} min - {} likes".format(self._nome, self.ano, self.duracao, self._likes)
+
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporada):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporada = temporada
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
+    def __str__(self):
+        return "{} - {} - {} tp - {} likes".format(self._nome, self.ano, self.temporada, self._likes)
 
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 vingadores.dar_like()
@@ -53,3 +50,8 @@ atlanta = Serie('atlanta', 2016, 2)
 atlanta.dar_like()
 
 print("Nome: {} - Ano {} - Temporadas:{} - Likes : {}".format(atlanta.nome, atlanta.ano, atlanta.temporada, atlanta.likes))
+
+filmes_e_serie = [vingadores, atlanta]
+
+for programa in filmes_e_serie:
+    print(programa)
