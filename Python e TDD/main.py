0a25a54ad5041d24b4c4d8f9f15a0117c9ced09f
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-03-11 19:45:05 -0300

    new course

--- Python e TDD/main.py
+++ Python e TDD/main.py
@@ -1,5 +1,12 @@
 from codigo.bytebank import Funcionario
 
-lucas = Funcionario("Lucas Carvalho", "13/03/2000", 1000)
+#lucas = Funcionario("Lucas Carvalho", "13/03/2000", 1000)
 
-print(lucas.idade())
+#print(lucas.idade())
+
+
+def teste_idade():
+    funcionario_teste = Funcionario('teste', "13/03/2000", 1111)
+    print('Teste igual = {}'.format(funcionario_teste.idade()))
+
+teste_idade()
